1f648864c6165693e120249324afe990bbe3d1aa
Catherine Beimfohr <cjbeimfohr13@gmail.com>
2021-01-14 17:33:20 -0500

    update app.py

--- app.py
+++ app.py
@@ -28,22 +28,37 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     
-    return "Welcome to my 'Home' page!"
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/names<br/>"
+        f"/api/v1.0/passengers"
+    )
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-  
-    return "Welcome to my 'About' page!"
+    
+    session = Session(engine)
+    
+    results = session.query(Measurement.date,Measurement.prcp).filter(Measurement.date>= "2016-08-23").\
+    filter(Measurement.date <="2017-08-23").all()
+    
+    session.close()
+    
+    return ""
 
 @app.route("/api/v1.0/stations")
 def stations():
-  
-    return "Welcome to my 'About' page!"
+    
+    session = Session(engine)
+    
+    return ""
 
 @app.route("/api/v1.0/tobs")
 def tobs():
-  
-    return "Welcome to my 'About' page!"
+    
+    session = Session(engine)
+    
+    return ""
 
 
 if __name__ == "__main__":
