8692ef9ce748d24e71bf90fc276a5d72632cf405
Catherine Beimfohr <cjbeimfohr13@gmail.com>
2021-01-14 17:59:17 -0500

    update app.py

--- app.py
+++ app.py
@@ -44,14 +44,31 @@ def precipitation():
     
     session.close()
     
-    return ""
+    precipitation = []
+    for date,prcp in results:
+        precipitation_dict = {}
+        precipitation_dict["date"] = date
+        precipitation_dict["prcp"] = prcp
+        precipitation.append(precipitation_dict)
+
+    return jsonify(precipitation)
+    
 
 @app.route("/api/v1.0/stations")
 def stations():
     
     session = Session(engine)
     
-    return ""
+    results = session.query(Measurement.station).group_by(Measurement.station).all()
+    
+    session.close()
+    
+    stations = [] 
+    for r in results: 
+        for x in r: 
+        stations.append(x) 
+        
+    return (stations)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
